3bb0d98308a06228a17ec8b5bd8002ecd395e744
roshy1234 <jamesroshy@gmail.com>
2023-12-09 16:44:24 +0530

    ssort

--- ssort.c
+++ ssort.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+
 void main() {
+    int n, A[20], small, temp;
 
-    int n,A[20],small,temp;
-
-    printf("Enter the number of elements: "); scanf("%d",&n);
-    printf("\n");
-
-    printf("Enter the elements of the array:");
-    for(int i=0;i<n;i++ )  scanf("%d",&A[i]);
-
-    
-    for(int i=0;i<n-1;i++) {
-        int m_pos= i;
-        for(int j=i+1;j<n;j++) {
-            if(A[j]<A[m_pos]) m_pos=j;
-            if(m_pos!=i) {
-            temp=A[m_pos];
-            A[m_pos]=A[i];
-            A[i]=temp;
-            }
-        }
-
-          
-        
-        
-
+    printf("Enter array size: ");
+    scanf("%d", &n);
+    printf("Enter the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
     }
 
-    for(int i=0;i<n;i++ )  printf("%d\t",A[i]);
+    for (int i = 0; i < n - 1; i++) {
+        int m_pos = i;
+        for (int j = i + 1; j < n; j++) {
+            if (A[j] < A[m_pos]) {
+                m_pos = j;
+            }
+        }
+        if (m_pos != i) {
+            temp = A[m_pos];
+            A[m_pos] = A[i];
+            A[i] = temp;
+        }
+        printf("Iteration %d : ", i + 1);
+        for (int i = 0; i < n; i++) {
+            printf("%d ", A[i]);
+        } 
+        printf("\n");
+    }
+
+    printf("The sorted array is: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
 }
